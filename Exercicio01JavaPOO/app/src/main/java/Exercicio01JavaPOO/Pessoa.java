b53b6dfb5c1a960ab93f17b288b79c5a3ac31be8
Gianbdf <gian.defaria@hotmail.com>
2023-04-01 22:47:47 -0300

    criando getters and setters

--- Exercicio01JavaPOO/app/src/main/java/Exercicio01JavaPOO/Pessoa.java
+++ Exercicio01JavaPOO/app/src/main/java/Exercicio01JavaPOO/Pessoa.java
@@ -22,5 +22,31 @@ public class Pessoa {
         this.dataNascimento = dataNascimento;
         this.altura = altura;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
     
 }
