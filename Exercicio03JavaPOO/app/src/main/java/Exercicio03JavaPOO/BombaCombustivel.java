7512a3d382b0b556760584519a4d7e72140aad1f
Gianbdf <gian.defaria@hotmail.com>
2023-04-02 18:16:28 -0300

    criando o método que altera o tipo de combustível, adicionando condicionais

--- Exercicio03JavaPOO/app/src/main/java/Exercicio03JavaPOO/BombaCombustivel.java
+++ Exercicio03JavaPOO/app/src/main/java/Exercicio03JavaPOO/BombaCombustivel.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Exercicio03JavaPOO;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +14,8 @@ public class BombaCombustivel {
     private char tipoCombustivel;
     private double valorPorLitro;
     private double quantidadeCombustivel;
+    
+    Scanner leitor = new Scanner(System.in);
     
     //calcula quantidade de combustivel pelo valor a ser abastecido
     public void abastecerPorValor(double valorParaAbastecer) {
@@ -29,5 +32,22 @@ public class BombaCombustivel {
     public void alterarValor(double novoValor) {
         this.valorPorLitro = novoValor;
         System.out.println("Novo valor por litro de combustivel: R$ " + novoValor);
+    }
+    
+    public void alterarCombustivel() {
+        System.out.println("Selecione o tipo de combustível: G - Gasolina, A - Alcool, D - Diesel");
+        char alterarCombustivel = leitor.next().charAt(0);
+        if (alterarCombustivel == 'G' || alterarCombustivel == 'g' ) {
+            this.tipoCombustivel = alterarCombustivel;
+            System.out.println("Tipo de combustivel: Gasolina");
+        } else if (alterarCombustivel == 'A' || alterarCombustivel == 'a' ) {
+            this.tipoCombustivel = alterarCombustivel;
+            System.out.println("Tipo de combustivel: Alcool");
+        } else if (alterarCombustivel == 'D' || alterarCombustivel == 'd' ) {
+            this.tipoCombustivel = alterarCombustivel;
+            System.out.println("Tipo de combustivel: Diesel");
+        } else {
+            System.out.println("Tipo de combustível invalido");
+        }
     }
 }
