d00bc84f1aa4d2f24f222d310725bfd14d104d39
Gianbdf <gian.defaria@hotmail.com>
2023-04-03 22:40:01 -0300

    formatando melhor as linhas

--- Exercicio04JavaPOO/app/src/main/java/Exercicio04JavaPOO/Elevador.java
+++ Exercicio04JavaPOO/app/src/main/java/Exercicio04JavaPOO/Elevador.java
@@ -11,48 +11,48 @@ import java.util.Scanner;
  * @author User
  */
 public class Elevador {
-    
+
     //atributos da classe
     private int andarAtual;
     private int totalAndares;
     private int capacidadeElevador;
     private int pessoasNoElevador;
-    
+
     Scanner leitor = new Scanner(System.in);
-    
-    public Elevador (int capacidadeElevador, int totalAndares){
+
+    public Elevador(int capacidadeElevador, int totalAndares) {
         this.capacidadeElevador = capacidadeElevador;
         this.totalAndares = totalAndares;
-    
+
     }
-    
-    public void entraPessoa () {
+
+    public void entraPessoa() {
         System.out.println("Informe a quantidade de pesssoas entrando no elevador");
         int quantidadePessoasEntrando = leitor.nextInt();
         if (quantidadePessoasEntrando < (this.capacidadeElevador - this.pessoasNoElevador)) {
             this.pessoasNoElevador += quantidadePessoasEntrando;
             System.out.println("Agora há " + this.pessoasNoElevador + " pessoas no elevador");
         } else {
-            System.out.println("Elevador só possui capacidade para mais " 
+            System.out.println("Elevador só possui capacidade para mais "
                     + (this.capacidadeElevador - this.pessoasNoElevador) + " pessoas.");
         }
     }
-    
-    public void saiPessoa () {
+
+    public void saiPessoa() {
         System.out.println("Informe a quantidade de pessoas saindo do elevador");
         int quantidadePessoasSaindo = leitor.nextInt();
         if (this.pessoasNoElevador > 0 && quantidadePessoasSaindo <= this.pessoasNoElevador) {
             this.pessoasNoElevador -= quantidadePessoasSaindo;
             System.out.println("Sairam " + quantidadePessoasSaindo + " pessoas "
-                    + "do elevador. Agora há " + this.pessoasNoElevador + " pessoas no elevador");       
+                    + "do elevador. Agora há " + this.pessoasNoElevador + " pessoas no elevador");
         } else {
             System.out.println("Valor inválido. Nao há ninguém dentro do elevador "
                     + "ou a quantidade de pessoas saindo e maior que a quantidade "
                     + "de pessoas no elevador, sendo invalido o valor.");
-        }   
+        }
     }
-    
-    public void subirAndarElevador () {
+
+    public void subirAndarElevador() {
         System.out.println("Informe quantos andares quer que o elevador suba");
         int quantidadeAndaresParaElevadorSubir = 0;
         quantidadeAndaresParaElevadorSubir = leitor.nextInt();
@@ -60,12 +60,12 @@ public class Elevador {
             this.andarAtual += quantidadeAndaresParaElevadorSubir;
             System.out.println("Você chegou ao andar " + this.andarAtual + "!");
         } else {
-            System.out.println ("Valor inválido. O predio possui apenas " 
-                    + this.totalAndares  + " andares! Voce está no andar " + this.andarAtual);
+            System.out.println("Valor inválido. O predio possui apenas "
+                    + this.totalAndares + " andares! Voce está no andar " + this.andarAtual);
         }
     }
-    
-    public void descerAndarElevador () {
+
+    public void descerAndarElevador() {
         System.out.println("Informe quantos andares quer que o elevador desça");
         int quantidadeAndaresParaElevadorDescer = 0;
         quantidadeAndaresParaElevadorDescer = leitor.nextInt();
@@ -73,9 +73,9 @@ public class Elevador {
             this.andarAtual -= quantidadeAndaresParaElevadorDescer;
             System.out.println("Você chegou ao andar " + this.andarAtual + "!");
         } else {
-            System.out.println ("Não existe andares abaixo do terreo."
+            System.out.println("Não existe andares abaixo do terreo."
                     + " Andar atual " + this.andarAtual);
         }
     }
-    
+
 }
