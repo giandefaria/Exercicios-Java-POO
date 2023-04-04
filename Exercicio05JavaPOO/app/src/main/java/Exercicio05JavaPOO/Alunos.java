689b47b1c7b7296e2f40d0464a1fbb1182c34418
Gianbdf <gian.defaria@hotmail.com>
2023-04-04 20:52:50 -0300

    corrigindo erro de digitação

--- Exercicio05JavaPOO/app/src/main/java/Exercicio05JavaPOO/Alunos.java
+++ Exercicio05JavaPOO/app/src/main/java/Exercicio05JavaPOO/Alunos.java
@@ -27,7 +27,7 @@ public class Alunos {
         setNotaProvaB(notaProvaB);
         setNotaTrabalho(notaTrabalho);
         System.out.println("Nome do aluno: " + nome);
-        Sytem.out.println("Matricula do aluno: " + matricula);
+        System.out.println("Matricula do aluno: " + matricula);
         System.out.println("Media final do aluno: " + mediaFinalAluno());
         System.out.println("Nota necessaria na prova final: " + notaNecessariaProvaFinal());
     }
