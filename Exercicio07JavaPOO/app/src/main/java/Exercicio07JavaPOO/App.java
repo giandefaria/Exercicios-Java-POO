366c008dd755a10cdfbf13025cb2ad941dc91987
Gianbdf <gian.defaria@hotmail.com>
2023-04-05 12:45:51 -0300

    arquivos iniciais do exercicio 07 de POO

---
+++ Exercicio07JavaPOO/app/src/main/java/Exercicio07JavaPOO/App.java
@@ -0,0 +1,21 @@
+/*
+A fim de representar funcionários em uma empresa, crie uma classe chamada Funcionario que inclui as
+três informações a seguir como atributos:
+a. um primeiro nome,
+b. um sobrenome
+c. um salário mensal
+Sua classe deve ter um construtor que inicializa os três atributos. Forneça os métodos getters e setters para
+cada atributo. Se o salário mensal não for positivo, configure-o como 0.0. Escreva um aplicativo de teste
+que demonstra as capacidades da classe. Crie duas instâncias da classe e exiba o salário anual de cada
+instância. Então dê a cada empregado um aumento de 10% e exiba novamente o salário anual de cada
+empregado.
+ */
+package Exercicio07JavaPOO;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
