a40b0e43fad94ca543e8c25e71aeff358ce69b51
Gianbdf <gian.defaria@hotmail.com>
2023-04-04 21:27:14 -0300

    adicionando atributos

---
+++ Exercicio06JavaPOO/app/src/main/java/Exercicio06JavaPOO/Invoice.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio06JavaPOO;
+
+/**
+ *
+ * @author User
+ */
+public class Invoice {
+    
+    private int numeroIten;
+    private String descricaoItem;
+    private int quantidade;
+    private double precoItem;
+    
+}
