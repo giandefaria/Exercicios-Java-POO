37675470d6a57420efa6639fbef115703c3d678b
Gianbdf <gian.defaria@hotmail.com>
2023-04-06 19:35:40 -0300

    criado a instancia data com os atributos privados

---
+++ Exercicio08JavaPOO/app/src/main/java/Exercicio08JavaPOO/Data.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio08JavaPOO;
+
+/**
+ *
+ * @author User
+ */
+public class Data {
+    
+    private int dia;
+    private int mes;
+    private int ano;
+    
+}
