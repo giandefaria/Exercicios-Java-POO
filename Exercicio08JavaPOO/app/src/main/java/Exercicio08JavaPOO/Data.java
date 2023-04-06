22caf8ea000dc790e389cb2d0c8f00bda7e85643
Gianbdf <gian.defaria@hotmail.com>
2023-04-06 19:36:40 -0300

    criando o metodo construtor

--- Exercicio08JavaPOO/app/src/main/java/Exercicio08JavaPOO/Data.java
+++ Exercicio08JavaPOO/app/src/main/java/Exercicio08JavaPOO/Data.java
@@ -14,4 +14,10 @@ public class Data {
     private int mes;
     private int ano;
     
+    public Data (int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
 }
