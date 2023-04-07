8cbd9cb4a7185c901f1c4beb6c419cdf4ac7071a
Gianbdf <gian.defaria@hotmail.com>
2023-04-07 18:22:27 -0300

    criada classe voo com os atributos iniciais

---
+++ Exercicio09JavaPOO/app/src/main/java/Exercicio09JavaPOO/Voo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio09JavaPOO;
+
+/**
+ *
+ * @author User
+ */
+public class Voo {
+    private int maximoPassageiros = 100;
+    private int numeroVoo;
+    private int vagasOcupadas;
+}
