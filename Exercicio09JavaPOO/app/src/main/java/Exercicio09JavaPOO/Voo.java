07c515467d623a070fad5ddb02f0ff4cdf76d8d7
Gianbdf <gian.defaria@hotmail.com>
2023-04-08 02:04:20 -0300

    indexando as poltronas

--- Exercicio09JavaPOO/app/src/main/java/Exercicio09JavaPOO/Voo.java
+++ Exercicio09JavaPOO/app/src/main/java/Exercicio09JavaPOO/Voo.java
@@ -12,11 +12,15 @@ public class Voo {
     private int maximoPassageiros = 100;
     private int numeroVoo;
     private int vagasOcupadas;
-    private int poltrona[] = new int[maximoPassageiros];
+    private String poltrona[] = new String[maximoPassageiros]; //vetor com numero de posições igual máximo passageiros
     
     public Voo(int numeroVoo, int diaVoo, int mesVoo, int anoVoo) {
         this.numeroVoo = numeroVoo;
         Data dataVoo = new Data(diaVoo, mesVoo, anoVoo);
+        
+        for (int i = 1; i <= this.maximoPassageiros; i++) {
+            this.poltrona[i] = "livre";
+        }
     }
     
 }
