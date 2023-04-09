fea1783c6c884200805b9989f112a6de107e130c
Gianbdf <gian.defaria@hotmail.com>
2023-04-09 20:32:23 -0300

    arquivos iniciais do exercicio 11 POO

---
+++ Exercicio11JavaPOO/app/src/main/java/Exercicio11JavaPOO/App.java
@@ -0,0 +1,15 @@
+/*
+Crie uma classe chamada Ingresso, que possui um valor em reais e um método imprimirValor. Crie uma
+classe IngressoVIP, que herda de Ingresso e possui um valor adicional. Crie um método que retorne o
+valor do ingresso VIP (com o adicional incluído). Crie um programa para criar as instâncias de Ingresso e
+IngressoVIP, mostrando a diferença de preços.
+ */
+package Exercicio11JavaPOO;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
