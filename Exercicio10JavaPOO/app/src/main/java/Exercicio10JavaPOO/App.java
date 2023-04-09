f839224a79b08005c06dd52a0c889693cd4d5f17
Gianbdf <gian.defaria@hotmail.com>
2023-04-09 14:47:37 -0300

    criada a classe 'jogador de futebol'

--- Exercicio10JavaPOO/app/src/main/java/Exercicio10JavaPOO/App.java
+++ Exercicio10JavaPOO/app/src/main/java/Exercicio10JavaPOO/App.java
@@ -6,6 +6,10 @@ c. data de nascimento;
 d. nacionalidade;
 e. altura;
 f. peso;
+Crie os métodos públicos necessários para getters e setters e também um método para imprimir todos os
+dados do jogador. Crie um método para calcular a idade do jogador e outro método para mostrar quanto
+tempo falta para o jogador se aposentar. Para isso, considere que os jogadores da posição de defesa se
+aposentam em média aos 40 anos, os jogadores de meio-campo aos 38 e os atacantes aos 35
  */
 package Exercicio10JavaPOO;
 
