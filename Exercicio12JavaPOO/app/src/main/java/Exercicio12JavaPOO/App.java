dbde67771ca71ce867fa6ec8dd3c1060e3ff3f7a
Gianbdf <gian.defaria@hotmail.com>
2023-04-10 06:14:51 -0300

    arquivos iniciais do exercicio 12 POO

---
+++ Exercicio12JavaPOO/app/src/main/java/Exercicio12JavaPOO/App.java
@@ -0,0 +1,18 @@
+/*
+Crie uma classe Agenda que pode armazenar 10 pessoas e que seja capaz de realizar as seguintes
+operações:
+a. void armazenarPessoa(String nome, int idade, float altura); //armazena a pessoa em um array
+b. void removerPessoa(String nome); //remove a pessoa do array
+c. int buscarPessoa(String nome); // informa em que posição da agenda está a pessoa
+d. void imprimirAgenda(); // imprime os dados de todas as pessoas da agenda
+e. void imprimirPessoa(int index); // imprime os dados da pessoa que está na posição “i” da agenda.
+ */
+package Exercicio12JavaPOO;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
