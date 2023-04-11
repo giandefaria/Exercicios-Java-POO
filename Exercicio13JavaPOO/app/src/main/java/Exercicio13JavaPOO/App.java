eb11e7a2b4efed54838df4424c23bdb88baaa5df
Gianbdf <gian.defaria@hotmail.com>
2023-04-10 22:40:04 -0300

    arquivos iniciais do exercicio 13 de POO

---
+++ Exercicio13JavaPOO/app/src/main/java/Exercicio13JavaPOO/App.java
@@ -0,0 +1,15 @@
+/*
+Crie uma classe Calculadora. Esta classe deve implementar as operações básicas (soma, subtração, divisão
+e multiplicação). Utilizando o conceito de herança crie uma classe chamada calculadora cientifica que
+implementa os seguintes cálculos: raizQuadrada e a potencia. Dica utilize a classe Math do pacote
+java.lang.
+ */
+package Exercicio13JavaPOO;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
