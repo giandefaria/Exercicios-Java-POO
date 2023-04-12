e590501d1ad629851ace14b7f8f9f053687b7e33
Gianbdf <gian.defaria@hotmail.com>
2023-04-12 20:50:12 -0300

    arquivos iniciais exercicio 15

---
+++ Exercicio15JavaPOO/app/src/main/java/Exercicio15JavaPOO/App.java
@@ -0,0 +1,20 @@
+/*
+Crie uma classe chamada Funcionário que inclui três atributos:
+a. nome (String);
+b. sobrenome (String);
+c. salário mensal (double).
+A classe deve ter um construtor, métodos getters e setters para cada atributo da classe. Escreva uma classe
+chamada FuncionarioTeste (que contenha o método main) que cria dois objetos da classe Funcionario e
+exibe o salário de cada objeto. Então dê a cada Funcionario um aumento de 10% e exiba novamente o
+salário anual de cada um deles. Introduza na classe Funcionario uma variável de classe capaz de
+contabilizar o numero de funcionarios que passaram pela empresa até a data.
+ */
+package Exercicio15JavaPOO;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
