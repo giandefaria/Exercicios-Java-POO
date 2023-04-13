4d3bda29ff9d885d5af988eeb90753d6815ad227
Gianbdf <gian.defaria@hotmail.com>
2023-04-12 21:25:44 -0300

    criado getters and setters

--- Exercicio15JavaPOO/app/src/main/java/Exercicio15JavaPOO/Funcionario.java
+++ Exercicio15JavaPOO/app/src/main/java/Exercicio15JavaPOO/Funcionario.java
@@ -12,5 +12,31 @@ public class Funcionario {
     private String nome;
     private String sobreNome;
     private double salarioMensal;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+    
+    
     
 }
