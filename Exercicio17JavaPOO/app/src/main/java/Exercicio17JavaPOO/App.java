2969f78047779a02d60b06c4ecf56ac850eab541
Gianbdf <gian.defaria@hotmail.com>
2023-04-14 19:58:46 -0300

    planificando a estrutura do projeto

--- Exercicio17JavaPOO/app/src/main/java/Exercicio17JavaPOO/App.java
+++ Exercicio17JavaPOO/app/src/main/java/Exercicio17JavaPOO/App.java
@@ -4,6 +4,8 @@ finalidade deve ser capaz de gerenciar compromissos, atribuindo a cada um o seu 
 entrega de projeto); data; nome do participante ( pessoa, empresa etc) alguém com quem acontecerá é o
 compromisso) e seu telefone. Desenvolva um sistema que seja capaz de fazer operações básicas como
 agendar, remover e alterar compromissos e exibir compromissos por participante e por data.
+*ideia, criar classe DadosGeraisAgendamento, que conterá a data, o nome do participanete, telefone, descrição;
+*a classe NovoAgendamento herdara a outra classe, e nela terá um sistema que perguntará o tipo de compromisso, perguntando se deseja criar outro ao final.
  */
 package Exercicio17JavaPOO;
 
