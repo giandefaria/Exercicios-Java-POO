1a6d6712bcff1c99988f748f79b9b49d8995a8a0
Gian de Faria <gian.defaria@hotmail.com>
2023-04-25 20:24:07 -0300

    aumentando em um o índice

--- Exercicio17JavaPOO/app/src/main/java/Exercicio17JavaPOO/CriarAgenda.java
+++ Exercicio17JavaPOO/app/src/main/java/Exercicio17JavaPOO/CriarAgenda.java
@@ -54,7 +54,7 @@ public class CriarAgenda {
                     case 2 -> dadosAgendamento[indice] = new DadosGeraisAgendamento("Pagamento", nome, telefone, descricao, dia, mes, ano);
                     default -> dadosAgendamento[indice] = new DadosGeraisAgendamento("Entrega de Projeto", nome, telefone, descricao, dia, mes, ano);
                 }
-                //adicionar indice
+                indice++;
             }else{
                 System.out.println("Valor inválido");
             }
